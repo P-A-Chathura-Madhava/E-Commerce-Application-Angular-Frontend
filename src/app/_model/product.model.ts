4ac3f9b296c82e7eaea1f63d27a5c30bcfa309b3
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-19 19:57:39 +0530

    apply add product feature

---
+++ src/app/_model/product.model.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    productName: String,
+    productDescription: String,
+    productDiscountedPrice: number,
+    productActualPrice: number
+}
--- src/app/add-new-product/add-new-product.component.ts
+++ src/app/add-new-product/add-new-product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../_model/product.model';
+import { NgForm } from '@angular/forms';
+import { ProductService } from '../_services/product.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-new-product',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewProductComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    productName: "",
+    productDescription: "",
+    productDiscountedPrice: 0,
+    productActualPrice: 0
+  }
+  
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+  }
+
+  addProduct(productForm: NgForm){
+    this.productService.addProduct(this.product).subscribe(
+      (response: Product)=>{
+        productForm.reset();
+      },
+      (error: HttpErrorResponse)=>{
+        console.log(error)
+      }
+      );
+    
   }
 
 }
