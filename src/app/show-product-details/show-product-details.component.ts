0745f394f5ae1e135618fb6e5d0f3d69933ed4d5
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-21 07:55:56 +0530

    apply edit product details feature

--- src/app/show-product-details/show-product-details.component.ts
+++ src/app/show-product-details/show-product-details.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from "@angular/material/dialog";
 import { ShowProductImagesDialogComponent } from "../show-product-images-dialog/show-product-images-dialog.component";
 import { ImageProcessingService } from "../image-processing.service";
 import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-show-product-details",
@@ -28,7 +29,8 @@ export class ShowProductDetailsComponent implements OnInit {
   constructor(
     private productService: ProductService,
     public imagesDialog: MatDialog,
-    private imageProcessingService: ImageProcessingService
+    private imageProcessingService: ImageProcessingService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -76,5 +78,9 @@ export class ShowProductDetailsComponent implements OnInit {
       height: "500px",
       width: "800px",
     });
+  }
+
+  editProductDetails(productId){
+    this.router.navigate(['/addNewProduct', {productId: productId}])
   }
 }
