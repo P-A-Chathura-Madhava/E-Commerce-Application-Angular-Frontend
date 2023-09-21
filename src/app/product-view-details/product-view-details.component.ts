55b78747aa1fc74606c01c4c032ed9594220f61c
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-21 17:25:47 +0530

    apply view product details feature

--- src/app/product-view-details/product-view-details.component.ts
+++ src/app/product-view-details/product-view-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../_model/product.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-view-details',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductViewDetailsComponent implements OnInit {
 
-  constructor() { }
+  selectProductIndex = 0;
+  product: Product;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.product = this.activatedRoute.snapshot.data['product'];
+  }
+
+  changeIndex(index){
+    this.selectProductIndex=index;
   }
 
 }
