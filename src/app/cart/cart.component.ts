fd3ddf8ec2e7f3d6f83ed6985d1ac80cf0b4a524
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-22 22:23:38 +0530

    apply checkout feature

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../_services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -11,7 +12,8 @@ export class CartComponent implements OnInit {
   displayedColumns: string[] = ['Name', 'Description', 'Price' , 'Discounted Price' ,'Action'];
   cartDetails : any[] = [];
 
-  constructor(private productService : ProductService) { }
+  constructor(private productService : ProductService,
+    private router : Router) { }
 
   ngOnInit(): void {
     this.getCartDetails();
@@ -27,6 +29,19 @@ export class CartComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  checkout(){
+    this.router.navigate(['/buyProduct', {
+      isSingleProductCheckout: false, id: 0
+    }]);
+    // this.productService.getProductDetails(false, 0).subscribe(
+    //   (resp) => {
+    //     console.log(resp);
+    //   },(error) =>{
+    //     console.log(error);
+    //   }
+    // );
   }
 
 }
